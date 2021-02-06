5218605705646f45ed8c7b3451863ef11e48140e
hedstromphilip <hedstromphilip@gmail.com>
2021-02-06 12:41:04 +0100

    changed name

--- src/components/team/index.js
+++ src/components/team/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Team as members } from 'src/constants';
+import { Team } from 'src/constants';
 
 import './team.css';
 
 const Team = () => {
     return (
         <div class="grid">
-            {Object.values(members).map((v,i) => {
+            {Object.values(Team).map((member,idx) => {
                 return (
-                    <div key={i.toString()} class="grid-item" style={{gridArea: v.gridId}}>
+                    <div key={idx.toString()} class="grid-item" style={{gridArea: member.gridId}}>
                         <div class="team-member">
-                            <p class="title team-member-name">{v.name}</p>
-                            <p class="subtitle team-member-role">{`${v.role}`}</p>
-                            <a href={`mailto:${v.mail}`}><i class="far fa-envelope mail-icon" /></a>
+                            <p class="title team-member-name">{member.name}</p>
+                            <p class="subtitle team-member-role">{`${member.role}`}</p>
+                            <a href={`mailto:${member.mail}`}><i class="far fa-envelope mail-icon" /></a>
                         </div>
                     </div>
                 );
